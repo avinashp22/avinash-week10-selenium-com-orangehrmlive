ddd8ccfe15eb3c2d416062d678288cb4f72c3029
avinashp22 <avinashp22@gmail.com>
2023-11-13 17:11:45 +0000

    avinash-week10-selenium-com-orangehrmlive

--- src/ComOrangehrmlive.java
+++ src/ComOrangehrmlive.java
@@ -9,7 +9,7 @@ import java.time.Duration;
 
 public class ComOrangehrmlive {
 
-    static String browser = "edge"; // choose browser
+    static String browser = "firefox"; // choose browser
     static WebDriver driver; // declare globally
 
     public static void main(String[] args) {
@@ -31,8 +31,8 @@ public class ComOrangehrmlive {
         System.out.println(driver.getTitle()); // print tile
         System.out.println(driver.getCurrentUrl()); // print current url
         System.out.println(driver.getPageSource()); // print page source
-        WebElement forgotLink = driver.findElement(By.linkText("Forgot your password?")); // hover to forgot pw link
-        forgotLink.click(); // click on link
+        // NOT WORKING -  WebElement forgotLink = driver.findElement(By.linkText("Forgot your password?")); // hover to forgot pw link
+        // NOT WORKING -   forgotLink.click(); // click on link
         System.out.println(driver.getCurrentUrl()); // print current url
         driver.navigate().back(); // go back a page to login page
         driver.navigate().refresh(); // refresh url
@@ -43,8 +43,12 @@ public class ComOrangehrmlive {
         WebElement password = driver.findElement(By.name("password"));       //find password field and enter password by ID
         password.sendKeys("admin123");    // type into box, action
 
-        WebElement loginLink = driver.findElement(By.className("oxd-button oxd-button--medium oxd-button--main orangehrm-login-button")); // hover to login button
-        loginLink.click(); // click on link
+        // NOT WORKING -  WebElement loginLink = driver.findElement(By.className("oxd-button oxd-button--medium oxd-button--main orangehrm-login-button")); // hover to login button
+        // NOT WORKING -  loginLink.click(); // click on link
+
+       // NOT WORKING -  WebElement loginLink2 = driver.findElement(By.linkText("Login")); // hover to login button
+        // NOT WORKING -   loginLink2.click(); // click on link
+
 
        driver.quit();  // close browser
     }
